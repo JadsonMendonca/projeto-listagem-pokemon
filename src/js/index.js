57e2817cebaa53e9371c87b82787596c70f00c2e
JadsonMendonca <50719658+JadsonMendonca@users.noreply.github.com>
2023-05-14 10:13:17 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,8 @@ botaoAlterarTema.addEventListener("click", () =>{
     const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
     body.classList.toggle("modo-escuro");
     if(modoEscuroEstaAtivo){
-        imagemBotaoTrocaDeTema.setAttribute("src", "src./imagens/sun.png");
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
     } else{
-        imagemBotaoTrocaDeTema.setAttribute("src", "src./imagens/moon.png");
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
     }
 });
